36e2125f7bae71034bbb678da62c9a3facd660ba
luojunhong0225 <1944993440@qq.com>
2020-10-09 16:11:40 +0800

    first commit

---
+++ spring-boot-restful-scaffold/src/test/java/com/luo/SpringBootRestfulScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulScaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
